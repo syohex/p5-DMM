91673d51ee3604d88ff733d39187636bec9f3030
Syohei YOSHIDA <syohex@gmail.com>
2012-09-29 06:42:33 +0900

    init repos

---
+++ lib/DMM.pm
@@ -0,0 +1,42 @@
+package DMM;
+use strict;
+use warnings;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+DMM -
+
+=head1 SYNOPSIS
+
+  use DMM;
+
+=head1 DESCRIPTION
+
+DMM is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
