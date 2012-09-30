a1c831663689fdfab4d2a4839994803bbb2ff75b
Syohei YOSHIDA <syohex@gmail.com>
2012-09-30 22:22:07 +0900

    add DMM::Ranking example

---
+++ eg/actress_ranking.pl
@@ -0,0 +1,25 @@
+#!perl
+use strict;
+use warnings;
+
+use lib "../lib";
+use DMM::Ranking;
+
+binmode STDOUT, ":utf8";
+
+my $dvd_ranking = DMM::Ranking->new('dvd');
+my $download_ranking = DMM::Ranking->new('download');
+
+my @actresses;
+@actresses = $dvd_ranking->actress_ranking(11, 20);
+
+my $rank = 11;
+for my $actress (@actresses) {
+    printf "[%2d] %s", $rank++, $actress->name;
+    if (@{$actress->aliases}) {
+        my @aliases = @{ $actress->aliases };
+        print "(@aliases)";
+    }
+
+    print "\n";
+}
