54e291048b5e1cdeabb0399d6ded52be981321d5
Syohei YOSHIDA <syohex@gmail.com>
2012-09-30 22:50:15 +0900

    fix initial value of index

--- eg/actress_ranking.pl
+++ eg/actress_ranking.pl
@@ -11,9 +11,10 @@ my $dvd_ranking = DMM::Ranking->new('dvd');
 my $download_ranking = DMM::Ranking->new('download');
 
 my @actresses;
-@actresses = $dvd_ranking->actress_ranking(41, 60);
+my ($min, $max) = (41, 60);
+@actresses = $dvd_ranking->actress_ranking($min, $max);
 
-my $rank = 11;
+my $rank = $min;
 for my $actress (@actresses) {
     printf "[%2d] %s", $rank++, $actress->name;
     if (@{$actress->aliases}) {
