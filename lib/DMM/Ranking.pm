007f59b7f62da61930e8e618dba9ac302fae2ac9
Syohei YOSHIDA <syohex@gmail.com>
2012-10-07 22:53:55 +0900

    remove code to subclass because it is base class

--- lib/DMM/Ranking.pm
+++ lib/DMM/Ranking.pm
@@ -2,105 +2,8 @@ package DMM::Ranking;
 use strict;
 use warnings;
 
-use 5.010;
-
-use Carp ();
-use Web::Scraper;
-use URI;
-use DMM::Actress;
-use DMM::Util qw/separate_name/;
-
-sub new {
-    my ($class, $media) = @_;
-
-    unless ($media ~~ ["dvd", "download"]) {
-        Carp::croak("'media' parameter should be 'dvd' or 'download'");
-    }
-
-    bless {
-        media => $media,
-    }, $class;
-}
-
-my %scraper = (
-    dvd => scraper {
-        process "table.mg-b20 > tr > td", "ranks[]" => scraper {
-            process "div.data > p > a", href => '@href', name => 'TEXT';
-        },
-    },
-);
-
-# Now download HTML structure is similar to DVD one
-$scraper{download} = $scraper{dvd};
-
-sub actress_ranking {
-    my ($self, $min, $max) = @_;
-
-    unless ($min >= 1) {
-        Carp::croak("'min' should be greater than equal '1'");
-    }
-
-    unless ($max <= 100) {
-        Carp::croak("'max' should be less than equal '100'");
-    }
-
-    if ($min > $max) {
-        Carp::croak("'min' should be greater than 'max'");
-    }
-
-    $min //= 1;
-    $max //= 100;
-
-    my $scraper = $scraper{ $self->{media} };
-
-    my @actresses;
-    for my $url ( $self->_actress_ranking_url($min, $max) ) {
-        my $res = $scraper->scrape( URI->new($url) );
-
-        for my $rank (@{$res->{ranks}}) {
-            my ($id) = $rank->{href} =~ m{id=(\d+)}g;
-            my ($name, $aliases_ref) = separate_name($rank->{name});
-
-            $aliases_ref //= [];
-
-            push @actresses, DMM::Actress->new(
-                name    => $name,
-                aliases => $aliases_ref,
-                id      => $rank->{id},
-            );
-        }
-    }
-
-    unless (@actresses) {
-        Carp::croak("Can't get any actress information");
-    }
-
-    return @actresses[0..($max - $min)];
-}
-
-sub _actress_ranking_url {
-    my ($self, $min, $max) = @_;
-    my $base = 'http://www.dmm.co.jp';
-
-    my ($min_page, $max_page) = map { ($_ - 1) / 20 } ($min, $max);
-
-    my @urls;
-    given ($self->{media}) {
-        when ('dvd') {
-            my $format = "${base}/mono/dvd/-/ranking/=/term=monthly/mode=actress/rank=%s/";
-            push @urls, map {
-                sprintf $format, $_;
-            } qw/1_20 21_40 41_60 61_80 81_100/;
-        }
-        when ('download') {
-            my $format = "${base}/digital/videoa/-/ranking_all/=/type=actress/page=%d/";
-            push @urls, map {
-                sprintf $format, $_;
-            } 1..5;
-        }
-    }
-
-    return @urls[$min_page..$max_page];
+sub ranking {
+    die "Please override 'ranking' method";
 }
 
 1;
@@ -113,30 +16,7 @@ __END__
 
 =head1 NAME
 
-DMM::Ranking - DMM ranking module
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-DMM::Ranking is ranking module.
-You can get actress ranking or product ranking.
-
-=head1 INTERFACES
-
-=head2 Class Methods
-
-=head3 C<< DMM::Ranking->new($media) >>
-
-Create and return a new DMM::Ranking instance.
-C<$media> parameter should be 'dvd' or 'download'.
-
-=head2 Instance method
-
-=head3 C<< $ranking->actress_ranking($min=1, $max=100)  >>
-
-Get actresses ranked from C<$min> to C<$max>(C<$max> should be greater than C<$min>).
-Each object returned is-a DMM::Actress.
+DMM::Ranking - Base class of DMM ranking class
 
 =head1 AUTHOR
 
